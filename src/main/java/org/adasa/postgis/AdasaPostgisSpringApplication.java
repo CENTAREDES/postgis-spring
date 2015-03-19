9767406e743f431b92e26e68baa23be5ec2e1aca
illan <cabox@box-codeanywhere.com>
2015-03-19 08:03:51 -0400

    first commit

---
+++ src/main/java/org/adasa/postgis/AdasaPostgisSpringApplication.java
@@ -0,0 +1,12 @@
+package org.adasa.postgis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdasaPostgisSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdasaPostgisSpringApplication.class, args);
+    }
+}
---
+++ src/test/java/org/adasa/postgis/AdasaPostgisSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package org.adasa.postgis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AdasaPostgisSpringApplication.class)
+public class AdasaPostgisSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
